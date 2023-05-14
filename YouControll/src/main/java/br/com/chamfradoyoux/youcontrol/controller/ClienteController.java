e2a270d5fe3839f81277fe27b65986ef5e0d1616
Chamfrado <prog.lohran@gmail.com>
2023-05-14 00:47:07 -0300

    completando o CRUD do nosso Cliente , e  arrumando um pouco a casa KKK

--- YouControll/src/main/java/br/com/chamfradoyoux/youcontrol/controller/ClienteController.java
+++ YouControll/src/main/java/br/com/chamfradoyoux/youcontrol/controller/ClienteController.java
@@ -1,10 +1,19 @@
 package br.com.chamfradoyoux.youcontrol.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import br.com.chamfradoyoux.youcontrol.model.Cliente;
 import br.com.chamfradoyoux.youcontrol.repository.ClienteRepository;
@@ -19,4 +28,59 @@ public class ClienteController {
     public List<Cliente> getAllClientes() {
         return clienteRepository.findAll();
     }
+
+
+
+
+
+
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id){
+        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+        if(clienteOptional.isPresent()){
+            Cliente cliente = clienteOptional.get();
+            return ResponseEntity.ok(cliente);
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping("/clientes")
+    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
+        Cliente createdCliente = clienteRepository.save(cliente);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(createdCliente.getId()).toUri();
+        return ResponseEntity.created(location).body(createdCliente);
+    }
+
+    @PutMapping("/clientes/update")
+    public ResponseEntity<Cliente> updateCliente(@RequestBody Cliente cliente) {
+        Optional<Cliente> clienteOptional = clienteRepository.findById(cliente.getId());
+        if(clienteOptional.isPresent()){
+            Cliente existingCliente = clienteOptional.get();
+            existingCliente.setNome(cliente.getNome());
+            existingCliente.setCnpj(cliente.getCnpj());
+            existingCliente.setEmail(cliente.getEmail());
+            existingCliente.setTelefone(cliente.getTelefone());
+            existingCliente.setUf(cliente.getUf());
+            existingCliente.setLocalizacao(cliente.getLocalizacao());
+            Cliente updatedCliente = clienteRepository.save(existingCliente);
+            return ResponseEntity.ok(updatedCliente);
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/clientes/delete/{id}")
+    public ResponseEntity<String> deleteCliente(@PathVariable Long id){
+        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+        if(clienteOptional.isPresent()){
+            clienteRepository.deleteById(id);
+            return ResponseEntity.ok("deletado com sucesso!");
+        } else{
+            System.out.println(id);
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- YouControll/src/main/java/br/com/chamfradoyoux/youcontrol/repository/ClienteRepository.java
+++ YouControll/src/main/java/br/com/chamfradoyoux/youcontrol/repository/ClienteRepository.java
@@ -5,8 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.chamfradoyoux.youcontrol.model.Cliente;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Object findById = null;
+
     List<Cliente> findAll();
+
+    Optional<Cliente> findById(Long id);
+
+    void deleteById(Long id);
+    
+    default Cliente create(Cliente cliente) {
+        return save(cliente);
+    }
+
+   
 }
