dbdaaa70a9d52b600fcda868401d69800e2868d0
Chamfrado <prog.lohran@gmail.com>
2023-05-23 10:04:57 -0300

    atualizando dados para colocar coordenadas

--- YouControll/BackEnd/src/main/resources/db/migration/V3__put_cliente_table_data.sql
+++ YouControll/BackEnd/src/main/resources/db/migration/V3__put_cliente_table_data.sql
@@ -1,12 +1,12 @@
 INSERT INTO cliente (nome, cnpj, email, telefone, uf, localizacao, created_at) 
 VALUES 
-  ('Pedro Silva', '32254763000198', 'pedro.silva@example.com', '11912345678', 'SP', 'São Paulo', NOW()),
-  ('Maria Souza', '91508172000106', 'maria.souza@example.com', '21945678901', 'RJ', 'Rio de Janeiro', NOW()),
-  ('José Santos', '77634259000140', 'jose.santos@example.com', '19998765432', 'SP', 'Campinas', NOW()),
-  ('Ana Oliveira', '50871483000171', 'ana.oliveira@example.com', '31987654321', 'MG', 'Belo Horizonte', NOW()),
-  ('Ricardo Pereira', '69438116000187', 'ricardo.pereira@example.com', '11976543210', 'SP', 'São Bernardo do Campo', NOW()),
-  ('Fernanda Costa', '05276129000102', 'fernanda.costa@example.com', '51912345678', 'RS', 'Porto Alegre', NOW()),
-  ('Gabriel Fernandes', '62426730000158', 'gabriel.fernandes@example.com', '27945678901', 'ES', 'Vitória', NOW()),
-  ('Letícia Lima', '08007011000109', 'leticia.lima@example.com', '81998765432', 'PE', 'Recife', NOW()),
-  ('Henrique Rodrigues', '86893259000129', 'henrique.rodrigues@example.com', '21987654321', 'RJ', 'Niterói', NOW()),
-  ('Carla Mendes', '39782228000103', 'carla.mendes@example.com', '11976543210', 'SP', 'São Paulo', NOW());
+  ('Pedro Silva', '32254763000198', 'pedro.silva@example.com', '11912345678', 'SP', POINT(-23.5505, -46.6333), NOW()),
+  ('Maria Souza', '91508172000106', 'maria.souza@example.com', '21945678901', 'RJ', POINT(-22.9068, -43.1729), NOW()),
+  ('José Santos', '77634259000140', 'jose.santos@example.com', '19998765432', 'SP', POINT(-22.9056, -47.0608), NOW()),
+  ('Ana Oliveira', '50871483000171', 'ana.oliveira@example.com', '31987654321', 'MG', POINT(-19.9227, -43.9451), NOW()),
+  ('Ricardo Pereira', '69438116000187', 'ricardo.pereira@example.com', '11976543210', 'SP', POINT(-23.6954, -46.5646), NOW()),
+  ('Fernanda Costa', '05276129000102', 'fernanda.costa@example.com', '51912345678', 'RS', POINT(-30.0346, -51.2177), NOW()),
+  ('Gabriel Fernandes', '62426730000158', 'gabriel.fernandes@example.com', '27945678901', 'ES', POINT(-20.3196, -40.3377), NOW()),
+  ('Letícia Lima', '08007011000109', 'leticia.lima@example.com', '81998765432', 'PE', POINT(-8.0578, -34.8829), NOW()),
+  ('Henrique Rodrigues', '86893259000129', 'henrique.rodrigues@example.com', '21987654321', 'RJ', POINT(-22.8832, -43.1039), NOW()),
+  ('Carla Mendes', '39782228000103', 'carla.mendes@example.com', '11976543210', 'SP', POINT(-23.5505, -46.6333), NOW());
